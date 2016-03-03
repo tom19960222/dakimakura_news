cde203a3bc6c01793ff872b05385eca4160ea9ee
爆頭專家 <tom19960222@gmail.com>
2016-03-03 11:07:49 +0800

    Update cron script, add template file and some test data.

--- get_this_month.sh
+++ get_this_month.sh
@@ -4,5 +4,5 @@ DATE=`date '+%Y%m'`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PYTHON3="/usr/bin/python3"
 
-$PYTHON3 $DIR/dakimakura_news.py $DATE > $DIR/rss/$DATE.xml
+$PYTHON3 $DIR/dakimakura_news.py $DATE > $DIR/rss/$DATE.json
 
